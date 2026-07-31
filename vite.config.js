827577509e91065971fa9f2848e804f4894b3523
Semara <darmaiputusemara@gmail.com>
2026-07-31 19:16:15 +0800

    Configure GitHub Pages automated deployment workflow and gh-pages package

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react"; // <-- tambahkan ini
+import react from "@vitejs/plugin-react";
 import path from "path";
 import { fileURLToPath } from "url";
 
 export default defineConfig({
-  plugins: [react()], // <-- tambahkan ini juga
+  base: "/Portfolio-Semara/",
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(path.dirname(fileURLToPath(import.meta.url)), "./src"),
